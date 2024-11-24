1b187d368a51d2cc06ec9f6e194515fb35e4aa90
Mirajul Islam <mirajulislam0411@gmail.com>
2024-11-25 00:57:34 +0600

    leap year

--- lab.c
+++ lab.c
@@ -1,25 +1,14 @@
+
 #include <stdio.h>
-void main()
+int main()
 {
-    int num1, num2, num3;
-
-    printf("Input the values of three numbers : ");
-    scanf("%d %d %d", &num1, &num2, &num3);
-
-
-    if (num1 > num2)   // Check if 'num1' is greater than 'num2'.
-    {
-        if (num1 > num3)   // Check if 'num1' is also greater than 'num3'.
-        {
-            printf("The 1st Number is the greatest among three. \n");
-        }
-        else
-        {
-            printf("The 3rd Number is the greatest among three. \n");
-        }
-    }
-    else if (num2 > num3)   // If 'num1' is not the greatest, check if 'num2' is greater than 'num3'.
-        printf("The 2nd Number is the greatest among three \n");
+    int Era;
+    printf("Enter the Era: ");
+    scanf("%d", &Era);
+    if ((Era %4 == 0 && Era %100 != 0) || (Era %400== 0))
+        printf   (" its Leap Year");
     else
-        printf("The 3rd Number is the greatest among three \n");  // If none of the above conditions are met, 'num3' must be the greatest.
+        printf   ( " its not a leap year");
+    return 0;
 }
+
